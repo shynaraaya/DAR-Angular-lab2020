7ff7abdd5da4ed03b094719cc22f16b851095ece
Ayanbek Shynara <39334586+shynaraaya@users.noreply.github.com>
2020-08-10 18:24:07 +0600

    fix(): passed logo through Input() into component

--- hw6/src/app/logo/logo.component.ts
+++ hw6/src/app/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent {
-  logo = '../assets/logo.png';
+
+  @Input()
+  public logo: string;
 }
