288ff43f568aa9e5cf199395d8b652f7a9808f54
Ayanbek Shynara <39334586+shynaraaya@users.noreply.github.com>
2020-08-20 14:32:17 +0600

    added logout() that clears cache

---
+++ hw9/src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from '@angular/core';
+import {Route, RouterModule} from '@angular/router';
+import {UsersComponent} from './users/users/users.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LayoutComponent} from './layout/layout.component';
+import {UserComponent} from './users/user/user.component';
+import {LoginComponent} from './auth/login/login.component';
+
+
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'users',
+        loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+      },
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      },
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule {}
